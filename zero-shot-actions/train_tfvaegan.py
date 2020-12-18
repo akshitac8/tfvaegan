be8f414fba2cb6b40eb5312de873efc2dc9a600a
Akshita Gupta <akshita.sem.iitr@gmail.com>
2020-12-18 16:47:12 +0400

    updated training file

--- zero-shot-actions/train_tfvaegan.py
+++ zero-shot-actions/train_tfvaegan.py
@@ -288,14 +288,12 @@ for epoch in range(0,opt.nepoch):
         # OD based GZSL
         seen_class = data.seenclasses.size(0)
         clsu = classifier.CLASSIFIER(syn_feature, util.map_label(syn_label, data.unseenclasses), data, data.unseenclasses.size(0), \
-                                    opt.cuda, _nepoch=25, _batch_size=opt.syn_num, use_dec=opt.zsl_dec, netDec=netDec, dec_size=opt.attSize, \
-                                    dec_hidden_size=4096, use_mult_rep=opt.use_mult_rep)
+                                    opt.cuda, _nepoch=25, _batch_size=opt.syn_num, netDec=netDec, dec_size=opt.attSize, dec_hidden_size=4096)
         clss = classifier.CLASSIFIER(data.train_feature, util.map_label(data.train_label,data.seenclasses), data, seen_class, opt.cuda, \
-                                    _nepoch=25, _batch_size=opt.syn_num, test_on_seen=True, use_dec=opt.zsl_dec, netDec=netDec, \
-                                    dec_size=opt.attSize, dec_hidden_size=4096, use_mult_rep=opt.use_mult_rep)
+                                    _nepoch=25, _batch_size=opt.syn_num, netDec=netDec, dec_size=opt.attSize, dec_hidden_size=4096)
         clsg = classifier_entropy.CLASSIFIER(data.train_feature, util.map_label(data.train_label,data.seenclasses), data, seen_class, \
-                                            syn_feature, syn_label, opt.cuda, clss, clsu, _batch_size=128, use_dec=opt.zsl_dec, \
-                                            netDec=netDec, dec_size=opt.attSize, dec_hidden_size=4096, use_mult_rep=opt.use_mult_rep)
+                                            syn_feature, syn_label, opt.cuda, clss, clsu, _batch_size=128, \
+                                            netDec=netDec, dec_size=opt.attSize, dec_hidden_size=4096)
         if best_gzsl_acc < clsg.H:
             best_acc_seen, best_acc_unseen, best_gzsl_acc = clsg.acc_seen, clsg.acc_unseen, clsg.H
         print('GZSL-OD: Acc seen=%.4f, Acc unseen=%.4f, h=%.4f' % (clsg.acc_seen, clsg.acc_unseen, clsg.H))
@@ -303,8 +301,8 @@ for epoch in range(0,opt.nepoch):
     # Zero-shot learning
     # Train ZSL classifier
     zsl_cls = classifier.CLASSIFIER(syn_feature, util.map_label(syn_label, data.unseenclasses), data, data.unseenclasses.size(0), \
-                                    opt.cuda, opt.classifier_lr, 0.5, 25, opt.syn_num, generalized=False, use_dec=opt.zsl_dec, netDec=netDec, \
-                                    dec_size=opt.attSize, dec_hidden_size=4096, use_mult_rep=opt.use_mult_rep)
+                                    opt.cuda, opt.classifier_lr, 0.5, 25, opt.syn_num, generalized=False, netDec=netDec, \
+                                    dec_size=opt.attSize, dec_hidden_size=4096)
     acc = zsl_cls.acc
     if best_zsl_acc < acc:
         best_zsl_acc = acc
